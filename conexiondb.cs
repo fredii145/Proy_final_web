73031cbee1b2b94c0896b3130ed57fad9ee93332
fredy Peralta <41094202+fredii145@users.noreply.github.com>
2021-11-14 20:38:44 -0600

    sqlserver

---
+++ conexiondb.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace deswebfinal.dbweb
+{
+    public class Conexiondb
+    {
+        string cadena = "Data Source=fredy5\\sqlserver;Initial Calago=dbweb; Integrated Security=True";
+        public SqlConnection conectarbd = new SqlConnection();
+
+        public Conexiondb()
+        {
+            conectarbd.ConnectionString = cadena;
+        }
+
+        public void Abrir()
+        {
+            try
+            {
+                conectarbd.Open();
+                Console.WriteLine("conexion abierta");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error al abrir "+ ex.Message);
+            }
+
+        }
+
+        public void Cerrar()
+        {
+            conectarbd.Close();
+        }
+
+    }
+
+
+}
